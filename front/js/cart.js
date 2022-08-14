a63e6ad8feb626e620f85aab23932bff00898562
sebneedles <sebneedles@gmail.com>
2022-08-14 09:06:57 +0200

    fonction suppression 1

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,6 +7,7 @@ console.log('Produits récupérés depuis localStorage =>', productTable);
 // Dans le sélecteur <section></section>, on affiche les produits
 const sectionProduct = document.querySelector('#cart__items');
 
+
 /* -------------------------------------------------------------------------------- 
 CONDITION
 -------------------------------------------------------------------------------- */
@@ -23,6 +24,26 @@ if(productTable === null || productTable == 0){
 } else { // Sinon panier contient un produit = afficher le produit
     console.log('Le panier n\'est plus vide :-)');
     //let ProductBasket = [];
+
+    /* -------------------------------------------------------------------------------- 
+    BOUTON SUPPRIMER
+    -------------------------------------------------------------------------------- */
+    function removeProduct(value){
+        console.log(value);
+        const removeItem = document.querySelectorAll(".deleteItem");
+        console.log('Ce bouton ...', removeItem);
+
+        for(let j = 0; j < removeItem.length; j++){
+            removeItem[j].addEventListener("click", (event) =>{
+                event.preventDefault();
+
+                let selectDeleteItem = productTable[j].id;
+                console.log('cest quoi ?', selectDeleteItem);
+
+            })
+        }
+    }
+    
 
     for(let i = 0; i < productTable.length; i++){
         // FETCH LE BON PRODUIT
@@ -112,18 +133,17 @@ if(productTable === null || productTable == 0){
             // <p class="deleteItem">Supprimer</p>
             const newParagrapheDelete = document.createElement('p');
             newParagrapheDelete.setAttribute("class", "deleteItem");
+            newParagrapheDelete.setAttribute("onclick", "removeProduct(this)");
             newDivDelete.appendChild(newParagrapheDelete);
             newParagrapheDelete.textContent = "Supprimer";
             
         })
         console.log('Nombre de produits dans le local storage : ', productTable.length);
     }
-    
 }
 
-
 /* -------------------------------------------------------------------------------- 
-BOUTON SUPPRIMER
+AFFICHAGE PRIX TOTAL
 -------------------------------------------------------------------------------- */
 
 
@@ -131,66 +151,16 @@ BOUTON SUPPRIMER
 
 
 
-
-
+    
 
 
 
 
 /* -------------------------------------------------------------------------------- 
-A REFAIRE CI DESSOUS
+GERER LA QUANTITE
 -------------------------------------------------------------------------------- */
 
-// MONTANT TOTAL DU PANIER
-let calculTotalPrice = [];
 
-// récupérer les prix du panier
-for(let k = 0; k < productTable?.length; k++){
-    let priceProductsBasket = productTable[k].price;
-
-    // Ajouter prix du panier dans la variable TotalPrice
-    calculTotalPrice.push(priceProductsBasket)
-}
-
-// Additionner les prix dans la variable TotalPrice
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
-const totalPrice = calculTotalPrice.reduce(reducer, 0);
-//console.log('Total du panier', totalPrice);
-
-// Affichage dans le DOM
-const displayTotalPrice = `<p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice">${totalPrice}</span> €</p>`
-const cartPrice = document.querySelector(".cart__price");
-cartPrice.innerHTML= displayTotalPrice;
-
-
-// FORMULAIRE DE COMMANDE
-const btnCommander = document.querySelector("#order");
-
-// Gestionnaire d'événnement
-btnCommander.addEventListener("click", (e) =>{
-    e.preventDefault(); // A RETIRER A LA FIN DU PROJET
-    
-    localStorage.setItem("firstName", document.querySelector("#firstName").value);
-    localStorage.setItem("lastName", document.querySelector("#lastName").value);
-    localStorage.setItem("address", document.querySelector("#address").value);
-    localStorage.setItem("city", document.querySelector("#city").value);
-    localStorage.setItem("email", document.querySelector("#email").value);
-
-    // Récupération des valeurs du formulaire dans un objet
-    const form = {
-        firstName: localStorage.getItem("firstName"),
-        lastName: localStorage.getItem("lastName"),
-        address: localStorage.getItem("address"),
-        city: localStorage.getItem("city"),
-        email: localStorage.getItem("email")
-    }
-    //console.log('données du formulaire', form);
-
-
-    // AJOUTER VALEURS DU FORMULAIRE ET PRODUITS SELECTIONNES VERS LE SERVEUR
-    const sendData = {
-        productTable,
-        form
-    }
-    //console.log('Envoi des données suivantes : =>', sendData);
-})
+/* -------------------------------------------------------------------------------- 
+FORMULAIRE DE COMMANDE
+-------------------------------------------------------------------------------- */
